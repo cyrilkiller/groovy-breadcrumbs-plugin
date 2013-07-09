d0922f96dc9ddb422b5815ca4d8498ca7e9a6016
Cyril <contact@cyril-leray.fr>
2013-07-10 01:25:36 +0200

    update test GEB

--- test/functional/breadcrumbs/plugins/pages/DemoPage.groovy
+++ test/functional/breadcrumbs/plugins/pages/DemoPage.groovy
@@ -4,15 +4,15 @@ import geb.Page
 
 class DemoPage extends Page {
 
-	static url = "http://localhost:8080/breadcrumbs-plugin/"
+	static url = "http://localhost:8080/groovy-breadcrumbs-plugin/"
 
 	static content = {
 		//activeItem {$(".breadcrumb > .active")}
 		breadcrumbsUl { $(".breadcrumb") }
 		li {$(breadcrumbsUl).find("li")}
 		active {$(breadcrumbsUl).find(".active")}
-		linkSubSubItem {$("a[href='/breadcrumbs-plugin/breadcrumbsDemo/pageThreeTwoTwo']")}
-		annoted {$("a[href='/breadcrumbs-plugin/breadcrumbsDemo/annoted']")}
-		custom {$("a[href='/breadcrumbs-plugin/breadcrumbsDemo/createsomething']")}
+		linkSubSubItem {$("a[href='/groovy-breadcrumbs-plugin/breadcrumbsDemo/pageThreeTwoTwo']")}
+		annoted {$("a[href='/groovy-breadcrumbs-plugin/breadcrumbsDemo/annoted']")}
+		custom {$("a[href='/groovy-breadcrumbs-plugin/breadcrumbsDemo/createsomething']")}
 	}
 }
